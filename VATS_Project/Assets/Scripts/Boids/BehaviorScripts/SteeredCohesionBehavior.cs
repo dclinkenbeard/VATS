46902c9bd36876d9fbb9aa12ccf44aef3eb7bb64
Isaac Torres <torresisaac26@gmail.com>
2020-09-09 15:29:56 -0500

    added strict avoidance behavior, obstacle avoidance, and organized

---
+++ VATS_Project/Assets/Scripts/Boids/BehaviorScripts/SteeredCohesionBehavior.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Flock/Behavior/Steered Cohesion")]
+public class SteeredCohesionBehavior : FilteredFlockBehavior
+{
+    Vector3 currentVelocity;
+    public float agentSmoothTime = 0.5f;
+    public override Vector3 CalculateMove(FlockAgent agent, List<Transform> context, Flock flock)
+    {
+        if (context.Count == 0)
+        {
+            return Vector3.zero;
+        }
+        Vector3 cohesionMove = Vector3.zero;
+        List<Transform> filteredContext = (filter == null) ? context : filter.Filter(agent, context);
+
+        foreach (Transform item in filteredContext)
+        {
+            cohesionMove += item.position;
+        }
+
+        cohesionMove /= context.Count;
+        cohesionMove -= agent.transform.position;
+
+        cohesionMove = Vector3.SmoothDamp(agent.transform.forward, cohesionMove, ref currentVelocity, agentSmoothTime);
+
+        return cohesionMove;
+    }
+}
---
+++ VATS_Project/Assets/Scripts/Boids/FilterScripts/PhysicsLayerFilter.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Flock/Filter/Physics Layer")]
+public class PhysicsLayerFilter : ContextFilter
+{
+    public LayerMask mask;
+
+    public override List<Transform> Filter(FlockAgent agent, List<Transform> original)
+    {
+        List<Transform> filtered = new List<Transform>();
+        foreach (Transform item in original)
+        {
+            //if (mask == (mask | (1 << item.gameObject.layer))) {
+            if (item.gameObject.layer == 8) {
+                filtered.Add(item);
+            }
+        }
+        foreach (Transform item in filtered) {
+            Debug.Log(item.gameObject);
+            Debug.Log(item.gameObject.name + "/n");
+        }
+
+        return filtered;
+    }
+}
