480bd4307766bf6c8ae78d918be25107a1903e24
dereki16 <derekiniguez@gmail.com>
2020-09-27 16:40:30 -0700

    implemented fev
    rough prototype of fev

---
+++ VATS_Project/Assets/Scenes/Derek_Scene/Scripts/fevUIList.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+[System.Serializable]
+public class fevUIList
+{
+    public string name;
+    public string diet;
+    public string status;
+    public string habitat;
+
+}
