f7b6550163935a0c7864eddffdaaed21aae6a9db
NikoCSUMB <70172259+NikoCSUMB@users.noreply.github.com>
2022-03-04 13:25:11 -0800

    Read Values From CSV File
     Prototype CSV parser that prints values from a sample document in the resources folder

---
+++ VATS_Project/Assets/Scripts/FEV/FEVParser.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+// Parser class used to read data from CSV files
+public class FEVParser : MonoBehaviour
+{
+    void Start()
+    {
+        // Load csv file from Resources folder
+        TextAsset fevFile = Resources.Load<TextAsset>("FEVs/sample1");
+
+        // Split document into rows
+        string[] fevLines = fevFile.text.Split("\n"[0]);
+
+        for (int i = 1; i < fevLines.Length; i++)
+        {
+            string[] fevData = (fevLines[i].Trim()).Split(","[0]);
+            for (int j = 1; j < fevData.Length; j++)
+            {
+                Debug.Log(fevData[j]);
+            }  
+        }
+    }
+
+    
+}
