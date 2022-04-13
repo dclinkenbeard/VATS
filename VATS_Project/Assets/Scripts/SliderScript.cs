1667f1bc3a004f94818241e48e95ac1752954391
Kyle Jackson <kyjackson@csumb.edu>
2022-04-13 12:34:25 -0700

    Split attributes into rates and static values
    The acidity slider is now modified by the acidity rate slider, and the acidity rate is correctly processed by a time function.

--- VATS_Project/Assets/Scripts/SliderScript.cs
+++ VATS_Project/Assets/Scripts/SliderScript.cs
@@ -17,21 +17,24 @@ public class SliderScript : MonoBehaviour
     //Declare sliders
     public UnityEngine.UI.Slider tempSlider;
     public UnityEngine.UI.Slider presSlider;
-    public UnityEngine.UI.Slider acidSlider;
+    public UnityEngine.UI.Slider aciditySlider;
+    public UnityEngine.UI.Slider acidityRateSlider;
     public UnityEngine.UI.Slider timeSlider;
     public UnityEngine.UI.Slider pollutionSlider;
 
     //Declare float variables
     public float tempValue;
     public float presValue;
-    public float acidValue;
+    public float acidityValue = 8.0f;
+    public float acidityRateValue = 0.0f;
     public float timeValue;
     public float pollutionValue;
 
     //Declare input field text values
     public InputField tempText;
     public InputField presText;
-    public InputField acidText;
+    public InputField acidityText;
+    public InputField acidityRateText;
     public InputField timeText;
     public InputField pollutionText;
 
@@ -40,7 +43,12 @@ public class SliderScript : MonoBehaviour
 
     private void Start()
     {
-        if(tempSlider == null || presSlider == null || acidSlider == null || timeSlider == null || pollutionSlider == null)
+        if(tempSlider == null 
+            || presSlider == null 
+            || aciditySlider == null 
+            || acidityRateSlider == null 
+            || timeSlider == null )
+            //|| pollutionSlider == null)
         {
             Debug.Log("ERROR: MISSING SLIDER GAME OBJECT IN SLIDERSCRIPT");
         }
@@ -48,9 +56,10 @@ public class SliderScript : MonoBehaviour
         {
             tempText.text = tempValue.ToString();
             presText.text = presValue.ToString();
-            acidText.text = acidValue.ToString();
+            acidityText.text = acidityValue.ToString();
+            acidityRateText.text = acidityRateValue.ToString();
             timeText.text = timeValue.ToString();
-            pollutionText.text = pollutionValue.ToString();
+            //pollutionText.text = pollutionValue.ToString();
         }
     }
 
@@ -106,39 +115,54 @@ public class SliderScript : MonoBehaviour
     public void AciditySlider(float newValue)
     {
         // rate of change in pH
-        acidValue = newValue;
-        acidSlider.value = acidValue;
-        acidText.text = acidValue.ToString();
-        fishManager.acidity += acidValue;
+        acidityValue = newValue;
+        aciditySlider.value = acidityValue;
+        acidityText.text = acidityValue.ToString();
+        fishManager.acidity = acidityValue;
         //Debug.Log(fishManager.acidity);
-    }
-
-    // Acidity Slider & Input Field
-    public void AcidityRateSlider(float newValue)
-    {
-        // rate of change in pH
-        acidValue = newValue;
-        acidSlider.value = acidValue;
-        acidText.text = acidValue.ToString();
-        fishManager.acidityRate = acidValue;
-        Debug.Log(fishManager.acidityRate);
     }
 
     /**
      * On Acidity slider value change, this function is called
      * parse float from string and set it as new value
-     * --- NOT IMPLEMENTED YET ---
      */
-    // public void AcidInput(string text)
-    // {
-    //     if (text == null)
-    //     {
-    //         text = "0";
-    //     }
-    //     float newValue = float.Parse(text);
-    //     newValue = CheckNewValue(acidSlider, newValue);
-    //     AciditySlider(newValue);
-    // }
+     // modifies the value in the input field next to the slider
+    public void AcidityInput(string text)
+    {
+        if (text == null)
+        {
+            text = "0";
+        }
+        float newValue = float.Parse(text);
+        newValue = CheckNewValue(aciditySlider, newValue);
+        AciditySlider(newValue);
+    }
+
+
+
+    // Acidity Slider & Input Field
+    public void AcidityRateSlider(float newValue)
+    {
+        // rate of change in pH
+        acidityRateValue = newValue;
+        acidityRateSlider.value = acidityRateValue;
+        acidityRateText.text = acidityRateValue.ToString();
+        fishManager.acidityRate = acidityRateValue;
+        //Debug.Log(fishManager.acidityRate);
+    }
+
+    public void AcidityRateInput(string text)
+    {
+        if (text == null)
+        {
+            text = "0";
+        }
+        float newValue = float.Parse(text);
+        newValue = CheckNewValue(acidityRateSlider, newValue);
+        AcidityRateSlider(newValue);
+    }
+
+
 
     // Pollution Slider & Input Field
     public void PollutionSlider(float newValue)
@@ -197,10 +221,16 @@ public class SliderScript : MonoBehaviour
     // On Apply Button
     public void OnApply()
     {
-        fishManager.SpawnFish();
-        Debug.Log("SENDING VALUES TO SCRIPT");
+
         fishManager.accountForTime();
-        Debug.Log(fishManager.acidity);
+        AciditySlider(fishManager.acidity);
+        AcidityInput(fishManager.acidity.ToString());
+        
+        fishManager.CalculateFish();
+        fishManager.SpawnFish();
+
+        // Debug.Log("SENDING VALUES TO SCRIPT");
+        // Debug.Log(fishManager.acidity);
     }
 
     /**
